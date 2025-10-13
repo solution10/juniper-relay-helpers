a5a50e1e799fd0a958c9d2c5c72ca184904731c2
Alex Gisby <alex@solution10.com>
2025-10-13 19:37:58 +1100

    feat!: better support for NoSQL style cursors

--- juniper_relay_helpers/src/cursor_provider.rs
+++ juniper_relay_helpers/src/cursor_provider.rs
@@ -1,3 +1,4 @@
+use crate::StringCursor;
 use juniper_relay_helpers::{Cursor, OffsetCursor, PageInfo, PageRequest};
 
 /// Struct that holds metadata about the response that can be used in the CursorProvider
@@ -15,21 +16,21 @@ pub struct PaginationMetadata {
 /// Cursor providers are how we generate cursors for each of the individual items
 /// within the result set, without needing to do a pass and build them manually.
 ///
-pub trait CursorProvider {
+pub trait CursorProvider<ItemT> {
     /// Build a cursor instance for the given item, with helper metadata etc.
     ///
     /// `metadata` is information about the current resultset we're building for.
     /// `item_idx` is the index of the item we're building a cursor for.
     /// `item` is the item itself.
-    fn get_cursor_for_item<T>(
+    fn get_cursor_for_item(
         &self,
         metadata: &PaginationMetadata,
         item_idx: i32,
-        item: &T,
+        item: &ItemT,
     ) -> impl Cursor;
 
     /// Builds the `PageInfo` to return to the RelayConnection
-    fn get_page_info<T>(&self, metadata: &PaginationMetadata, items: &[T]) -> PageInfo;
+    fn get_page_info(&self, metadata: &PaginationMetadata, items: &[ItemT]) -> PageInfo;
 }
 
 // -------------- OffsetCursorProvider ---------------
@@ -37,12 +38,12 @@ pub trait CursorProvider {
 /// Built-in cursor provider that can handle Offset cursors. Serves as a reference implementation for
 /// your own cursor providers too.
 pub struct OffsetCursorProvider;
-impl CursorProvider for OffsetCursorProvider {
-    fn get_cursor_for_item<T>(
+impl<ItemT> CursorProvider<ItemT> for OffsetCursorProvider {
+    fn get_cursor_for_item(
         &self,
         metadata: &PaginationMetadata,
         item_idx: i32,
-        _item: &T,
+        _item: &ItemT,
     ) -> impl Cursor {
         // OK this is annoying. If there _was_ a cursor passed to `after`, the offset needs to start
         // at the next item. If there wasn't, the offset needs to start at the first item (0).
@@ -69,7 +70,7 @@ impl CursorProvider for OffsetCursorProvider {
         }
     }
 
-    fn get_page_info<T>(&self, metadata: &PaginationMetadata, items: &[T]) -> PageInfo {
+    fn get_page_info(&self, metadata: &PaginationMetadata, items: &[ItemT]) -> PageInfo {
         let default_cursor = OffsetCursor::default();
         let current_cursor = match &metadata.page_request {
             Some(pr) => match pr.parsed_cursor() {
@@ -127,6 +128,70 @@ impl OffsetCursorProvider {
     /// if we need to pass things into it.
     pub fn new() -> Self {
         OffsetCursorProvider
+    }
+}
+
+// ------------- Keyed cursor provider -------------
+
+/// Trait to implement to use with items in the `KeyedCursorProvider`.
+trait CursorByKey {
+    fn cursor_key(&self) -> String;
+}
+
+/// This cursor provider is for working with something like DynamoDB. Each item's cursor is generated
+/// by implementing the `CursorByKey` trait, and the PageInfo is generated using the item cursors themselves.
+///
+/// If any `after` is provided, it's assumed that there is a previous page.
+/// If there are any items returned, it's assumed that there is a next page.
+///
+/// NOTE - read that previous line again. This follows the style of opaque, web scale cursors where the only
+/// valid last page is an empty page. This can be unexpected to a lot of frontends.
+pub struct KeyedCursorProvider;
+
+impl<ItemT> CursorProvider<ItemT> for KeyedCursorProvider
+where
+    ItemT: CursorByKey,
+{
+    fn get_cursor_for_item(
+        &self,
+        _metadata: &PaginationMetadata,
+        _item_idx: i32,
+        item: &ItemT,
+    ) -> impl Cursor {
+        StringCursor::new(item.cursor_key())
+    }
+
+    fn get_page_info(&self, metadata: &PaginationMetadata, items: &[ItemT]) -> PageInfo {
+        let mut first_item_cursor: Option<String> = None;
+        let mut last_item_cursor: Option<String> = None;
+
+        if let Some(first_item) = items.first() {
+            first_item_cursor = Some(
+                self.get_cursor_for_item(metadata, 0, first_item)
+                    .to_encoded_string(),
+            );
+        }
+
+        if let Some(last_item) = items.last() {
+            last_item_cursor = Some(
+                self.get_cursor_for_item(metadata, items.len() as i32 - 1, last_item)
+                    .to_encoded_string(),
+            );
+        }
+
+        let mut has_previous_page = false;
+        if let Some(pr) = &metadata.page_request {
+            if let Some(_) = &pr.after {
+                has_previous_page = true;
+            }
+        }
+
+        PageInfo {
+            start_cursor: first_item_cursor,
+            end_cursor: last_item_cursor,
+            has_prev_page: has_previous_page,
+            has_next_page: items.len() > 0,
+        }
     }
 }
 
@@ -390,6 +455,138 @@ mod tests {
                     .to_encoded_string()
                 )
             );
+        }
+    }
+
+    mod keyed_cursor_provider {
+        use crate::{
+            Cursor, CursorProvider, KeyedCursorProvider, PageRequest, PaginationMetadata,
+            RelayConnection, StringCursor,
+        };
+        use juniper::GraphQLObject;
+        use juniper_relay_helpers::cursor_provider::CursorByKey;
+
+        #[derive(Debug, Clone, GraphQLObject, RelayConnection, Eq, PartialEq)]
+        pub struct NoSQLItem {
+            id: String,
+        }
+        impl CursorByKey for NoSQLItem {
+            fn cursor_key(&self) -> String {
+                self.id.to_string()
+            }
+        }
+
+        #[test]
+        fn test_item_cursors() {
+            let p = KeyedCursorProvider;
+            let items = vec![
+                NoSQLItem {
+                    id: "id-1".to_string(),
+                },
+                NoSQLItem {
+                    id: "id-2".to_string(),
+                },
+                NoSQLItem {
+                    id: "id-3".to_string(),
+                },
+            ];
+
+            let meta = PaginationMetadata {
+                total_count: 3,
+                page_request: Some(PageRequest::new(
+                    Some(10),
+                    Some(StringCursor::new("".to_string())),
+                )),
+            };
+
+            let i1_cursor = p.get_cursor_for_item(&meta, 0, &items[0]);
+            assert_eq!(i1_cursor.to_encoded_string(), "c3RyaW5nOmlkLTE=");
+
+            let i2_cursor = p.get_cursor_for_item(&meta, 1, &items[1]);
+            assert_eq!(i2_cursor.to_encoded_string(), "c3RyaW5nOmlkLTI=");
+
+            let i3_cursor = p.get_cursor_for_item(&meta, 2, &items[2]);
+            assert_eq!(i3_cursor.to_encoded_string(), "c3RyaW5nOmlkLTM=");
+        }
+
+        #[test]
+        fn test_page_info_full_page() {
+            let p = KeyedCursorProvider {};
+            let items = vec![
+                NoSQLItem {
+                    id: "id-1".to_string(),
+                },
+                NoSQLItem {
+                    id: "id-2".to_string(),
+                },
+                NoSQLItem {
+                    id: "id-3".to_string(),
+                },
+            ];
+
+            let meta = PaginationMetadata {
+                total_count: 3,
+                page_request: Some(PageRequest {
+                    first: Some(10),
+                    after: None,
+                }),
+            };
+
+            let page_info = p.get_page_info(&meta, &items);
+            assert!(!page_info.has_prev_page);
+            assert!(page_info.has_next_page); // assume next is true due to items being returned.
+            assert_eq!(page_info.start_cursor, Some("c3RyaW5nOmlkLTE=".to_string()));
+            assert_eq!(page_info.end_cursor, Some("c3RyaW5nOmlkLTM=".to_string()));
+        }
+
+        #[test]
+        fn test_page_info_first_page_of_many() {
+            let p = KeyedCursorProvider {};
+            let items = vec![
+                NoSQLItem {
+                    id: "id-1".to_string(),
+                },
+                NoSQLItem {
+                    id: "id-2".to_string(),
+                },
+                NoSQLItem {
+                    id: "id-3".to_string(),
+                },
+            ];
+
+            let meta = PaginationMetadata {
+                total_count: 30, // More than the items returned, we have more items
+                page_request: Some(PageRequest {
+                    first: Some(10),
+                    after: None,
+                }),
+            };
+
+            let page_info = p.get_page_info(&meta, &items);
+            assert!(!page_info.has_prev_page);
+            assert!(page_info.has_next_page);
+            assert_eq!(page_info.start_cursor, Some("c3RyaW5nOmlkLTE=".to_string()));
+            assert_eq!(page_info.end_cursor, Some("c3RyaW5nOmlkLTM=".to_string()));
+        }
+
+        #[test]
+        fn test_page_info_last_page() {
+            let p = KeyedCursorProvider {};
+            let items: Vec<NoSQLItem> = vec![];
+
+            let meta = PaginationMetadata {
+                total_count: 30, // More than the items returned, we have more items
+                page_request: Some(PageRequest {
+                    first: Some(10),                             // More than items returned
+                    after: Some("c3RyaW5nOmlkLTA=".to_string()), // id-0 - we're paginating.
+                }),
+            };
+
+            let page_info = p.get_page_info(&meta, &items);
+            assert!(page_info.has_prev_page);
+            assert!(!page_info.has_next_page);
+            assert_eq!(page_info.start_cursor, None);
+            assert_eq!(page_info.end_cursor, None);
         }
     }
 }
